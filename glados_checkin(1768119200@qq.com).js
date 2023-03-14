93748992c503d37b10059bb73c66cb9443b0f30d
TryAgainC <63404307+TryAgainC@users.noreply.github.com>
2023-03-14 20:19:23 +0800

    Update glados_checkin(1768119200@qq.com).js

--- glados_checkin(1768119200@qq.com).js
+++ glados_checkin(1768119200@qq.com).js
@@ -87,6 +87,7 @@ function signin() {
       Origin: `https://glados.rocks`,
       "Accept-Encoding": `gzip, deflate, br`,
       Cookie: `__stripe_mid=6a1e7100-ffb3-4cdc-bc3e-b310a9b883a8588442; Cookie=enabled; Cookie.sig=lbtpENsrE0x6riM8PFTvoh9nepc; _gid=GA1.2.1625162287.1678684918; _ga=GA1.1.1901055235.1668352081; _ga_CZFVKMNT9J=GS1.1.1678772588.5.1.1678774569.0.0.0; koa:sess=eyJ1c2VySWQiOjk4Mzk0LCJfZXhwaXJlIjoxNzA0NzA2MjQxNjY0LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=FTMK2_nTivvoB-6eewEbxJdiuE4`,
+      "Content-Type": `application/json;charset=utf-8`,
       Host: `glados.rocks`,
       Connection: `keep-alive`,
       "User-Agent": `Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1`,
